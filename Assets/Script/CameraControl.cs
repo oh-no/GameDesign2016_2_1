ae7520fdafc74a3c5831f7035537f09627d37ae3
oh-no <amemiya.0207@gmail.com>
2017-01-18 22:22:04 +0900

    カメラ正常に

--- Assets/Script/CameraControl.cs
+++ Assets/Script/CameraControl.cs
@@ -5,9 +5,15 @@ public class CameraControl : MonoBehaviour {
 	[SerializeField]Transform target;
 	[SerializeField]float spinSpeed = 1.0f;
 
+	private const float DISTANCE = 0.01f;	//カメラとターゲットの距離
+
 	Vector3 nowPos;
 	Vector3 pos = Vector3.zero;
 	Vector2 mouse = Vector2.zero;
+	Vector3 oldPos = Vector3.zero;
+	bool isCol = false;
+	float oldMouseX;
+	float oldMouseY;
 	// Use this for initialization
 	void Start () {
 		// Canera get Start Position from Player
@@ -24,23 +30,46 @@ public class CameraControl : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (!isCol) {
+			// Get MouseMove
+			mouse += new Vector2 (oldMouseX, oldMouseY) * Time.deltaTime * spinSpeed;
+		} else {
+			// Get MouseMove
+			oldMouseY -= 1;
+			mouse -= new Vector2 (oldMouseX, oldMouseY) * Time.deltaTime * spinSpeed;
+			isCol = false;
+		}
 
-		// Get MouseMove
-		mouse += new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * Time.deltaTime * spinSpeed;
 		// Clamp mouseY move
-		mouse.y = Mathf.Clamp(mouse.y, -0.3f + 0.5f, 0.3f + 0.5f);
-
+		mouse.y = Mathf.Clamp (mouse.y, -0.3f + 0.5f, 0.3f + 0.5f);
 		// sphere coordinates
-		pos.x = Mathf.Sin(mouse.y * Mathf.PI) * Mathf.Cos(mouse.x * Mathf.PI) ;
-		pos.y = Mathf.Cos(mouse.y * Mathf.PI) ;
-		pos.z = Mathf.Sin(mouse.y * Mathf.PI) * Mathf.Sin(mouse.x * Mathf.PI) ;
+		pos.x = Mathf.Sin (mouse.y * Mathf.PI) * Mathf.Cos (mouse.x * Mathf.PI);
+		pos.y = Mathf.Cos (mouse.y * Mathf.PI);
+		pos.z = Mathf.Sin (mouse.y * Mathf.PI) * Mathf.Sin (mouse.x * Mathf.PI);
 		// r and upper
 		pos *= nowPos.z;
 
 		pos.y += nowPos.y;
 		//pos.x += nowPos.x; // if u need a formula,pls remove comment tag.
+		oldPos = pos;
 
-		transform.position = pos + target.position;
+		transform.position = DISTANCE * pos + target.position;
 		transform.LookAt(target.position);
+
+		//プレイヤーの向きを変える
+		Vector3 lookRot = 2 * target.position - transform.position;
+		lookRot.y = target.transform.position.y;
+		target.LookAt (lookRot);
+
+
+		// Get MouseMove
+		oldMouseX = Input.GetAxis ("Mouse X");
+		oldMouseY = Input.GetAxis ("Mouse Y");
+	}
+
+	void OnCollisionStay(Collision other)
+	{
+		isCol = true;
+		Debug.Log (other);
 	}
 }
